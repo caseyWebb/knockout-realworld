18043614f1f9d641f727be4d443b78a7c5373079
Casey Webb <notcaseywebb@gmail.com>
2018-02-14 00:59:52 -0600

    Persist home page view to querystring

--- src/views/home/viewModel.ts
+++ src/views/home/viewModel.ts
@@ -1,5 +1,6 @@
 import * as ko from 'knockout'
 import { ViewModelConstructorBuilder } from '@profiscience/knockout-contrib-model'
+import Query from '@profiscience/knockout-contrib-querystring'
 import { ArticlesModel } from 'lib/models/article'
 import { TagsModel } from 'lib/models/tags'
 import { currentUser } from 'lib/models/user'
@@ -7,28 +8,33 @@ import { currentUser } from 'lib/models/user'
 export default class extends ViewModelConstructorBuilder {
   public currentUser = currentUser
   
-  private query = {
-    feed: ko.observable(currentUser.loggedIn()),
-    tag: ko.observable('')
-  }
-  public articles = new ArticlesModel(this.query)
+  private query = new Query({
+    feed: currentUser.loggedIn() ? 'user' : 'global',
+    tag: ''
+  })
+  public articles = new ArticlesModel(ko.pureComputed(() => ({
+    feed: this.query.feed() === 'user',
+    tag: this.query.tag()
+  })))
   public tags = new TagsModel({})
 
-  public isGlobalFeed = ko.pureComputed(() => !this.query.feed() && !this.query.tag())
+  public showTabs = ko.pureComputed(() => currentUser.loggedIn() || this.query.feed() === 'tag')
+
+  public isGlobalFeed = ko.pureComputed(() => this.query.feed() === 'global')
   public showGlobalFeed() {
-    this.query.feed(false)
-    this.query.tag('')
+    this.query.clear()
+    this.query.feed('global')
   }
 
-  public isUserFeed = this.query.feed
+  public isUserFeed = ko.pureComputed(() => this.query.feed() === 'user')
   public showUserFeed() {
     if (!this.currentUser.loggedIn()) return
-    this.query.feed(true)
-    this.query.tag('')
+    this.query.clear()
+    this.query.feed('user')
   }
 
   public showTag(tag: string) {
-    this.query.feed(false)
+    this.query.feed('tag')
     this.query.tag(tag)
   }
 }
